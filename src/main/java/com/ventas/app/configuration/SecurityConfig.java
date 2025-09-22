3ffdc63a149dc856811c5e45addcfa542095db5b
Jorge Ramirez <jlramirez1210@gmail.com>
2025-09-21 23:14:22 -0500

    config security

---
+++ src/main/java/com/ventas/app/configuration/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.ventas.app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.extern.slf4j.Slf4j;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Slf4j
+@Configuration
+public class SecurityConfig {
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		log.info("defaultSecurityFilterChain...");
+	    http
+	        .authorizeHttpRequests((requests) -> requests
+	            .requestMatchers("/api/v1/categoria/*").authenticated()
+	            .requestMatchers("api/v1/producto/*").permitAll())
+	        //.formLogin(withDefaults()) 
+	        .httpBasic(withDefaults()); 
+	    return http.build();
+	}
+	
+	@Bean
+	UserDetailsService userDetailsService() {
+		log.info("userDetailsService...");
+		UserDetails admin = User.builder()
+			.username("admin")
+			.password("{noop}adminpass")
+			.roles("ADMIN", "USER")
+			.build();
+		UserDetails user = User.builder() 
+			.username("user")
+			.password("{noop}password")
+			.roles("USER")
+			.build();
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+}
